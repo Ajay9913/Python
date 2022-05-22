eaf99c77324ea10c853c4bade70714c0a227b86a
Ajay Bandgar <88834198+Ajay9913@users.noreply.github.com>
2022-05-22 18:46:45 +0530

    Find Max number using Max function

---
+++ Python Codes/find_max.py
@@ -0,0 +1,17 @@
+'''Given two numbers, write a Python code to find the Maximum of these two numbers.
+
+Examples: 
+
+Input: a = 2, b = 4
+Output: 4
+
+Input: a = -1, b = -4
+Output: -1'''
+
+a = -2
+b = -4
+c = -6
+d = -3
+
+maximum = max(a,b,c,d)
+print(maximum)
