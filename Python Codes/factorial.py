154d0444a1f823e5d32dd37891c51ed0b70f0a50
Ajay Bandgar <88834198+Ajay9913@users.noreply.github.com>
2022-05-22 18:53:32 +0530

    find factorial number

---
+++ Python Codes/factorial.py
@@ -0,0 +1,47 @@
+'''Python Program for factorial of a number
+
+Factorial of a non-negative integer, is multiplication of all integers smaller than or equal to n. For example factorial of 6 is 6*5*4*3*2*1 which is 720.'''
+
+import math
+
+#create function
+def factorial(n):
+    return(math.factorial(n))
+
+
+#main code
+num = 1
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 2
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 3
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 4
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 5
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 6
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 7
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 8
+print("factorial of", num, "is",
+      factorial(num))
+
+num = 9
+print("factorial of", num, "is",
+      factorial(num))
